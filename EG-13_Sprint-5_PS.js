16987c5c91e7941138bc19efbb788ca027202422
SportNest <student@sportnest.com>
2026-09-18 23:05:07 +0600

    solved the number of problem 01

---
+++ EG-13_Sprint-5_PS.js
@@ -0,0 +1,21 @@
+// 01. Remove Duplicates from Sorted Array
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
+// let arr1 = [1,1,2,2,5];
+// console.log(removeDuplicates(arr1));
+
